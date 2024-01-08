64652fb861254e472af8dacd433cb38582ecbc67
Connor <batmanual9595@gmail.com>
2024-01-08 17:46:45 -0500

    did intake util and created Teleop class By Connor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.utilities.Intake;
+
+@Config
+@TeleOp(name="DriveTrain Teleop")
+public class Teleop extends OpMode {
+
+}
